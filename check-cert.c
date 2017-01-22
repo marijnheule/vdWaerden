c086793f17cd9eed59bfd7956eec8095b1ae483d
marijn <marijn@marijns-MacBook-Pro.local>
2017-01-22 12:58:46 -0600

    check-cert done

--- check-cert.c
+++ check-cert.c
@@ -4,7 +4,7 @@
 #define INIT	10
 
 int main (int argc, char** argv) {
-  int i, j, tmp, max = 0;
+  int i, j, k, tmp, max = 0;
   int *cert, size, allocated;
   FILE *input;
 
@@ -15,18 +15,27 @@ int main (int argc, char** argv) {
   allocated = INIT;
   cert = (int*) malloc (sizeof (int) * allocated);
 
+  size = 0;
   input = fopen (argv[1], "r");
   while (1) {
     char c;
     tmp = fscanf (input, "%c", &c);
     if (tmp == EOF) break;
-    cert[size++] = (int) (c - 'a');
+    if (((int) (c - 'a')) >= 0)
+      cert[size++] = (int) (c - 'a');
     if (cert[size - 1] > max) max = cert[size - 1];
     if (size == allocated) {
       allocated *= 2;
       cert = (int *) realloc (cert, sizeof(int) * allocated); } }
   fclose (input);
 
-  printf("c parsed certificate using %i sets and %i numbers\n", max + 1, size);
+  printf ("c parsed certificate using %i sets and %i numbers\n", max + 1, size);
 
-}
+  max = 1;
+  for (i = 0; i < size; i++)
+    for (j = 1; (i + j*max) < size; j++)
+      for (k = 1; (i + j*k) < size; k++) {
+        if (cert[i+j*k] != cert[i]) break;
+        if (k + 1 > max) max = k + 1; }
+
+  printf ("c largest arithmetic progression is %i\n", max); }
